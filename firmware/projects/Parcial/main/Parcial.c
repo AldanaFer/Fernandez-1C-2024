4fb2b846573dcdaf0654df7059c4cd0f02e042b5
AldanaFer <aldanaagfernandez@gmail.com>
2024-06-11 11:42:36 -0300

    Agrego el proyecto del Parcial en blanco y ya habiendo realizado el primer build

---
+++ firmware/projects/Parcial/main/Parcial.c
@@ -0,0 +1,40 @@
+/*! @mainpage Parcial
+ *
+ * @author Aldana Agustina Fernandez (aldana.fernandez@ingenieria.uner.edu.ar)
+ *
+ * @section Parcial
+ *
+ * @section Descripcion General
+ * 
+ * 
+ * 
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/06/2024 | Document creation		                         |
+ * | 11/06/2024 | Document completion		                     |
+ *
+ *
+ */
+
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+	
+}
+/*==================[end of file]============================================*/
