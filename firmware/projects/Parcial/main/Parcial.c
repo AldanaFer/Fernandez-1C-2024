97368508987a8339e5d93ffb97b95a0e7442c3ca
AldanaFer <aldanaagfernandez@gmail.com>
2024-06-11 13:42:46 -0300

    Agrego la tarea mostrar, y agrego los comentarios necesarios para la documentacion

--- firmware/projects/Parcial/main/Parcial.c
+++ firmware/projects/Parcial/main/Parcial.c
@@ -6,13 +6,15 @@
  *
  * @section Descripcion General
  *  El sistema está compuesto por una serie de recipientes con agua, una solución de 
- * pH ácida y otra básica, un sensor de húmedad y uno de pH, y tres bombas
- *  peristálticas para los líquidos.
+ *  pH ácida y otra básica, un sensor de húmedad y uno de pH, y tres bombas
+ *  peristálticas para los líquidos. Hay que encenderlas o apagarlas segun lo que 
+ *  devuelvan los sensores. 
+ *  Ademas hay que mostrar la informacion por UART.
+ *  Y mediente los switch 1 y 2 de la placa controlar si el sistema esta encendido
+ *  o apagado.
  * 
  * 
  * 
- * @section hardConn Hardware Connection
- *
  * @section hardConn Hardware Connection
  *
  * |   Perifericos  |   ESP32   	|
@@ -51,14 +53,14 @@
 /*==================[internal data definition]===============================*/
 TaskHandle_t mostrar_task_handle = NULL;
 TaskHandle_t medir_task_handle = NULL;
-static bool sistema = true;
-static bool bomba_agua = false; 
-static bool bomba_acido = false;
-static bool bomba_base = false;
+static bool sistema = true;         // indica si el sistema esta prendido o apagado
+static bool bomba_agua = false;     // indica si la bomba de agua esta prendida o apagada
+static bool bomba_acido = false;    // indica si la bomba acida esta prendida o apagada
+static bool bomba_base = false;     // indica si la bomba basica esta prendida o apagada
 float pH;
 /*==================[internal functions declaration]=========================*/
 /**
- * @brief Función invocada en la interrupción del timer A, para convertir a analogico
+ * @brief Función invocada en la interrupción del timer A, para mostrar la informacion
  */
 void FuncTimerMostrar(void* param)
 {
@@ -66,7 +68,7 @@ void FuncTimerMostrar(void* param)
 }
 
 /**
- * @brief Función invocada en la interrupción del timer B, para convertir a digital.
+ * @brief Función invocada en la interrupción del timer B, para medir las varaibles
  */
 void FuncTimerMedir(void* param)
 {
@@ -74,7 +76,7 @@ void FuncTimerMedir(void* param)
 }
 
 /**
- * @brief Tarea encargada de leer el dato analogico y mandarlo como string 
+ * @brief Tarea encargada de medir las variables y prender o apagar las bombas que corresponden
  */
 static void taskMedir(void *pvParameter)
 {
@@ -88,7 +90,7 @@ static void taskMedir(void *pvParameter)
             pH = (float)dato * (14.0/3.0) ;  // como el voltaje es de 0 a 3, y 0V es 0 pH, y 3V es 14 pH, el factor de conversion es 14/3
             if(pH < 6.0)
             {
-                //prendo bobmba basica y apago la otra por las dudas 
+                //prendo bobmba basica y apago la acida
                 GPIOOn(GPIO_23);
                 GPIOOff(GPIO_22);
                 bomba_acido = false;
@@ -97,7 +99,7 @@ static void taskMedir(void *pvParameter)
             else 
             if(pH > 6.7)
             {
-                //prondo bomba acida y apago la otra por las dudas
+                //prondo bomba acida y apago la basica
                 GPIOOn(GPIO_22);
                 GPIOOff(GPIO_23);
                 bomba_base = false;
@@ -127,7 +129,7 @@ static void taskMedir(void *pvParameter)
 };
 
 /**
- * @brief Tarea encargada de leer el dato analogico y mandarlo como string 
+ * @brief Tarea encargada de mostrar la informacion a traves de la UART
  */
 static void taskMostrar(void *pvParameter)
 {
@@ -136,7 +138,33 @@ static void taskMostrar(void *pvParameter)
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 
         if (sistema)
         {
+            UartSendString(UART_PC,"pH: ");
+            UartSendString(UART_PC, (const char* )  UartItoa( pH, 10)); 
 
+            if(bomba_agua)
+            {
+               UartSendString(UART_PC,", humedad correcta "); 
+            }
+            else 
+            {
+                UartSendString(UART_PC,", humedad incorrecta ");
+            }
+			UartSendString(UART_PC, "\r\n");
+
+            if(bomba_acido)
+            {
+                UartSendString(UART_PC,"Bomba de pH acido encendida."); 
+            }
+            if(bomba_base)
+            {
+                UartSendString(UART_PC,"Bomba de pH basico encendida."); 
+            }
+
+            if(bomba_agua)
+            {
+                UartSendString(UART_PC,"Bomba de agua encendida."); 
+            }
+			UartSendString(UART_PC, "\r\n");
         }
 	}
 };
@@ -200,9 +228,7 @@ void app_main(void)
 		.param_p = NULL
 	}; 
 	UartInit(&miUart);
-
-
-
+    
 	xTaskCreate(&taskMostrar, "Mostrar", 4096, NULL, 5, &mostrar_task_handle);
 	xTaskCreate(&taskMedir, "Medir", 4096, NULL, 5, &medir_task_handle);
     xTaskCreate(&taskSwitch, "Switch", 1024, NULL, 5, NULL);
